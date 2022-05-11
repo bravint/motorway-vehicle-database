7b3ae5f6462c95c2d360886084573a79ecc430c4
bravint <bravint@gmail.com>
2022-05-11 18:34:22 +0100

    add postgres variables

--- config/config.js
+++ config/config.js
@@ -1,23 +1,25 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+require('dotenv').config();
+
+module.exports = {
+development: {
+    username: process.env.DB_DEV_USERNAME,
+    password: process.env.DB_DEV_PASSWORD,
+    database: process.env.DB_DEV,
+    host: process.env.DB_DEV_HOST,
+    dialect: 'postgres',
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: process.env.DB_TEST_USERNAME,
+    password: process.env.DB_TEST_PASSWORD,
+    database: process.env.DB_TEST,
+    host: process.env.DB_TEST_HOST,
+    dialect: 'postgres',
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    username: process.env.DB_PROD_USERNAME,
+    password: process.env.DB_PROD_PASSWORD,
+    database: process.env.DB_PROD,
+    host: process.env.DB_PROD_HOST,
+    dialect: 'postgres',
   }
 }
