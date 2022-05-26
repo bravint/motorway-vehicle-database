be45e1b640c24afea3749a0c5db903d050ec127b
bravint <bravint@gmail.com>
2022-05-26 11:08:06 +0100

    add extentions

--- src/controllers/vehicles.js
+++ src/controllers/vehicles.js
@@ -7,10 +7,45 @@ const getVehicles = async (req, res, next) => {
     try {
         const fetchedVehicles = await vehicles.findAll();
 
+        const clonedVehicles = [...fetchedVehicles];
+
+        const newClonedVehicles = clonedVehicles.map((vehicles) => vehicles.toJSON());
+
+        const makeNewVehicleObject = (vehicle) => {
+            return {
+                id: vehicle.id,
+                make: vehicle.make,
+                model: vehicle.model,
+                makeModel: (vehicle.makeModel = `${vehicle.make} - ${vehicle.model}`),
+            };
+        };
+
+        const amendedVehicle = newClonedVehicles.map((vehicle) => makeNewVehicleObject(vehicle));
+
         res.status(SERVER_SUCCESS.OK.CODE).json({
             status: SERVER_SUCCESS.OK.STATUS,
-            data: fetchedVehicles,
+            data: amendedVehicle,
         });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const deleteVehicleById = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+
+        //console.log(id);
+
+        const deletedVehicle = await vehicles.destroy({
+            where: {
+                id: Number(id),
+            },
+        });
+
+        console.log('deletedVehicle', deletedVehicle);
+
+        res.sendStatus(SERVER_SUCCESS.OK.CODE);
     } catch (error) {
         next(error);
     }
@@ -18,4 +53,5 @@ const getVehicles = async (req, res, next) => {
 
 module.exports = {
     getVehicles,
+    deleteVehicleById,
 };
--- src/routes/vehicles.js
+++ src/routes/vehicles.js
@@ -2,8 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-const { getVehicles } = require('../controllers/vehicles');
+const { getVehicles, deleteVehicleById } = require('../controllers/vehicles');
 
 router.get('/', getVehicles);
+router.delete('/:id', deleteVehicleById)
 
 module.exports = router;
